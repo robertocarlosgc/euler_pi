c042747a49e398362bfc8ab71318b24cbe9012e6
robertocarlosgc <robertocarlosgc04@gmail.com>
2022-11-27 21:28:46 -0600

    Go files started

---
+++ Go/eulerpi_serial_go.go
@@ -0,0 +1,30 @@
+// =================================================================
+//
+// File: eulerpi_serial_go.go
+// Author(s):
+//          Roberto Carlos Guzmán Cortés A01702388
+//
+// Description: This file contains the code to calculate an
+//              approximation of pi using the euler series
+//              algorithm. How to compile:
+//              go run eulerpi_serial_go.go
+//
+// SIZE = 100_000_000
+// Serial time: 
+//
+// Copyright (c) 2020 by Tecnologico de Monterrey.
+// All Rights Reserved. May be reproduced for any non-commercial
+// purpose.
+//
+// =================================================================
+
+package main
+
+import(
+	"fmt"
+)
+
+func main(){
+	result := 0.0
+	fmt.Printf("%f", result)
+}
