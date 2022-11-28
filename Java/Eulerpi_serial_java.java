135f0670ec1ce7e75a8664700be1e579f8bd052a
robertocarlosgc <robertocarlosgc04@gmail.com>
2022-11-27 20:08:58 -0600

    java files renamed to start with capital letter

---
+++ Java/Eulerpi_serial_java.java
@@ -0,0 +1,20 @@
+// =================================================================
+//
+// File: eulerpi_serial_c.c
+// Author(s):
+//          Roberto Carlos Guzmán Cortés A01702388
+//
+// Description: This file contains the code to calculate an
+//              approximation of pi using the euler series
+//              algorithm. How to compile using math.h library:
+//              gcc eulerpi_serial_c.c -lm
+//
+// SIZE = 100_000_000
+// Serial time: 1,527.14200 ms
+//
+// Copyright (c) 2020 by Tecnologico de Monterrey.
+// All Rights Reserved. May be reproduced for any non-commercial
+// purpose.
+//
+// =================================================================
+
